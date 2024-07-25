9f9e1abd2a96b1a4cf52af9935fbf433975a4bb1
Alejo Sosa <alejososa1987@gmail.com>
2024-07-25 01:17:56 -0300

    cambio en el style de navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,34 @@
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { CartWidget } from "./CartWidget";
 
 export const NavBar = () => {
     return (
-            <Navbar bg="light" data-bs-theme="light">
-                <Container>
-                    <Navbar.Brand href="#home">Abama Store</Navbar.Brand>
+        <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
+            <Container>
+                <Navbar.Brand href="#home">ABAMA</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Remeras</Nav.Link>
-                        <Nav.Link href="#features">Buzos</Nav.Link>
-                        <Nav.Link href="#pricing">Lycras</Nav.Link>
+                        <Nav.Link href="#home">INICIO</Nav.Link>
+                        <Nav.Link href="#link">ACTIVIDADES</Nav.Link>
+                        <NavDropdown title="STORE" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">REMERAS</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">BUZOS </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">LYCRAS</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">BERMUDAS</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.5">CONTACTO</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
-            <CartWidget />
-                </Container>
-            </Navbar>
-
+                </Navbar.Collapse>
+                <CartWidget/>
+            </Container>
+        </Navbar>
     );
 };
+
+
